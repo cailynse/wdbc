446b4e48c1fd96eb3356814774007c87947525cd
cailynse <cailyn.s.e@gmail.com>
2018-01-22 15:06:23 -0500

    started array assignment

---
+++ todoList/javascript.js
@@ -0,0 +1,17 @@
+var todos = [];
+
+var input = prompt("What would you like to do?")
+
+while (input !== "quit") {
+
+    if (input === "list") {
+        console.log(todos);
+
+    } else if (input === "new") {
+        var newTodo = prompt("Enter a new todo");
+        todos.push(newTodo);
+    }
+    input = prompt("What would you like to do?")
+}
+
+console.log("OK, you quit the app")
