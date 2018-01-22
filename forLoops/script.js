138ab8fcc51ed8a29fcee73513bb4e996d55ba22
cailynse <cailyn.s.e@gmail.com>
2018-01-22 09:35:25 -0500

    Added files to be tracked, solved first two problems

---
+++ forLoops/script.js
@@ -0,0 +1,13 @@
+console.log("Printing problem one");
+
+for (var i = -10; i < 20; i++) {
+    console.log(i);
+}
+
+console.log("Printing Problem 2");
+
+for (var h = 10; h <= 40; h++) {
+    if (h % 2 === 0) {
+        console.log(h);
+    }
+}
