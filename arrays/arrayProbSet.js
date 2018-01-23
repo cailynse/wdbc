f1e69559cae2ac434ed220be6b37230a31dda3fd
cailynse <cailyn.s.e@gmail.com>
2018-01-23 12:33:44 -0500

    Finished array problem set

--- arrays/arrayProbSet.js
+++ arrays/arrayProbSet.js
@@ -1,6 +1,6 @@
 function printReverse(arr) {
     //get the last index
-    var i = arr.length - 1
+    var i = arr.length - 1;
 
     //as long as the index is 0 or higher print the value of the index
     arr.forEach(function (reverse) {
@@ -9,4 +9,40 @@ function printReverse(arr) {
             i--
         }
     });
+}
+
+
+function isUniform(arr) {
+    //check that all items are the same
+
+    var lastIndex = arr.length - 1;
+
+    for (var i = 0; i < lastIndex; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function sumArray(arr) {
+
+    var sum = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+
+function max(arr) {
+    var max = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
