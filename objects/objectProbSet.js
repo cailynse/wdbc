03f5ea229c8cea1da9703da9e27619ab19befc33
cailynse <cailyn.s.e@gmail.com>
2018-01-24 12:40:35 -0500

    finished object assignment MovieDB

---
+++ objects/objectProbSet.js
@@ -0,0 +1,32 @@
+var movieDB = [
+    {
+        title: "Moulin Rouge",
+        rating: "*****",
+        hasWatched: true
+    },
+    {
+        title: "The Green Mile",
+        rating: "****",
+        hasWatched: false
+    },
+    {
+        title: "Patch Adams",
+        rating: "*****",
+        hasWatched: true
+    },
+    {
+        title: "All Dogs go to Heaven",
+        rating: "*****",
+        hasWatched: true
+    }
+        ]
+
+function movieReport(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i]["hasWatched"] === true) {
+            console.log("You have watched " + arr[i]["title"] + " - " + arr[i]["rating"]);
+        } else {
+            console.log("You have not watched " + arr[i]["title"] + " - " + arr[i]["rating"]);
+        }
+    }
+}
