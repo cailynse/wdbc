b19330fb7280e54de9ba67d489cfcf4ccffed339
cailynse <cailyn.s.e@gmail.com>
2018-01-24 16:53:40 -0500

    complete first DOM selector exercise

---
+++ domManipulation/domSelectors.js
@@ -0,0 +1,6 @@
+//Come up with 4 ways to slect the first <p> tag
+
+var one = document.getElementById("first");
+var two = document.getElementsByClassName("special")[0];
+var three = document.querySelector(".special");
+var four = document.getElementsByTagName("p")[0];
