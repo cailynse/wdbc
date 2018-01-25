e260f29114a2a071b133306a5958d38fefcaff02
cailynse <cailyn.s.e@gmail.com>
2018-01-25 18:48:37 -0500

    Partway through Score Keeper code along

--- domManipulation/colorChanger.js
+++ domManipulation/colorChanger.js
@@ -1,6 +1,12 @@
 alert("Connected");
+
+//select buttom
 var btn = document.querySelector("button");
+
+//select body
 var page = document.querySelector("body");
+
+//when button is clicked if purpleBAckground is not already a class add it, if it is remove it (toggle)
 btn.addEventListener("click", function () {
     console.log("clicked");
     page.classList.toggle("purpleBackground");
