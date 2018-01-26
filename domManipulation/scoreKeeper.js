e13901d2bb400921377543f2f6ca53334975a017
cailynse <cailyn.s.e@gmail.com>
2018-01-26 09:07:04 -0500

    last touches on score keeper code along

--- domManipulation/scoreKeeper.js
+++ domManipulation/scoreKeeper.js
@@ -14,8 +14,20 @@ var gameOver = false;
 
 var winningScore = 5;
 
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
 numInput.addEventListener("change", function () {
     numDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
 
 })
 
@@ -42,12 +54,6 @@ p2Button.addEventListener("click", function () {
 })
 
 resetButton.addEventListener("click", function () {
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove("winner");
-    p2Display.classList.remove("winner");
-    gameOver = false;
+    reset();
 
 })
