a9be9b51b20c1e2015bc599e13e38c1427877d31
cailynse <cailyn.s.e@gmail.com>
2018-01-30 13:58:04 -0500

    Finshed up Color Game styling

--- domManipulation/colorGame.js
+++ domManipulation/colorGame.js
@@ -28,7 +28,7 @@ easyBtn.addEventListener("click", function () {
             squares[i].style.display = "none";
         }
     }
-    h1.style.backgroundColor = "#232323"
+    h1.style.backgroundColor = "steelblue"
     resetButton.textContent = "New Colors";
 });
 
@@ -48,7 +48,7 @@ hardBtn.addEventListener("click", function () {
         squares[i].style.backgroundColor = colors[i];
         squares[i].style.display = "block";
     }
-    h1.style.backgroundColor = "#232323"
+    h1.style.backgroundColor = "steelblue"
     resetButton.textContent = "New Colors";
 });
 
@@ -97,7 +97,7 @@ resetButton.addEventListener("click", function () {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323"
+    h1.style.backgroundColor = "steelblue"
     resetButton.textContent = "New Colors";
 })
 
