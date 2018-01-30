0abba4a5c8a5c96df8ad5c4ed32548d93957d308
cailynse <cailyn.s.e@gmail.com>
2018-01-30 11:59:55 -0500

    Added New Colors/Play Again Button, and white banner

--- domManipulation/colorGame.js
+++ domManipulation/colorGame.js
@@ -1,10 +1,10 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 function generateRandomColors(num) {
     //make an array
@@ -23,6 +23,7 @@ function randomColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
+    //return a rgb value
     return ("rgb(" + r + ", " + g + ", " + b + ")");
 }
 
@@ -39,6 +40,21 @@ function changeColors(color) {
     }
 }
 
+resetButton.addEventListener("click", function () {
+    //generate new set of colors
+    colors = generateRandomColors(6);
+    //pick new color
+    pickedColor = pickColor();
+    //change text to display color
+    colorDisplay.textContent = pickedColor;
+    //display new colors
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323"
+    resetButton.textContent = "New Colors";
+})
+
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -53,6 +69,7 @@ for (var i = 0; i < squares.length; i++) {
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again"
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
